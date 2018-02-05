4809281f9b17f60dc19ec42fb0a98611f6ab9633
Jelizaveta Potapova <j.potapova@replyltd.co.uk>
2018-02-05 16:55:37 +0200

    setup pf lint ignore build and watch

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import { render } from 'react-dom';
+
 const ce = React.createElement;
 const MyTitle = function(props) {
   return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
@@ -12,7 +14,8 @@ const MyFirstComponent = function() {
     ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
     ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
     ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'peru' })
+    ce(MyTitle, { title: 'Silicon Valley', color: 'peru' }),
+		ce(MyTitle, { title: 'House of Cards', color: 'peru' })
   );
 };
 
