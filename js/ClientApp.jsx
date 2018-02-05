678360da134c8900a3eab800a1a458e450a91c87
Jelizaveta Potapova <j.potapova@replyltd.co.uk>
2018-02-05 15:59:21 +0200

    babel config

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 const ce = React.createElement;
 const MyTitle = function(props) {
   return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
@@ -8,7 +8,7 @@ const MyTitle = function(props) {
 const MyFirstComponent = function() {
   return ce(
     'div',
-    { id: 'my-first-component' },
+    { id: 'my-first-component'},
     ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
     ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
     ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
