b04111c6fa4757f1084130a02d187a3b5138ea15
Jelizaveta Potapova <j.potapova@replyltd.co.uk>
2018-02-05 17:49:31 +0200

    set up a webapp

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -2,21 +2,14 @@ import React from 'react';
 
 import { render } from 'react-dom';
 
-const ce = React.createElement;
-const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
-};
+const App = () => (
+  <div className="app">
+    <div className="landing">
+      <h1>svideo</h1>
+      <input type="text" placeholder="search" />
+      <a>or Browse All</a>
+    </div>
+  </div>
+);
 
-const MyFirstComponent = function() {
-  return ce(
-    'div',
-    { id: 'my-first-component'},
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'peru' }),
-		ce(MyTitle, { title: 'House of Cards', color: 'peru' })
-  );
-};
-
-render(ce(MyFirstComponent), document.getElementById('app'));
+render(<App />, document.getElementById('app'));
