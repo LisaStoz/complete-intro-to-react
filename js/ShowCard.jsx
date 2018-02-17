89a808b43af0a687e860f6cfb3f7fdd15d99daeb
Jelizaveta Potapova <j.potapova@replyltd.co.uk>
2018-02-17 22:21:28 +0200

    spread operator

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,29 +1,22 @@
 import React from 'react';
-import { shape, string } from 'prop-types';
+import { string } from 'prop-types';
 
 const ShowCard = props => (
   <div className="show-card">
-    <img alt={`${props.show.title} Show Poster`} src={`/public/img/posters/${props.show.poster}`} />
+    <img alt={`${props.title} Show Poster`} src={`/public/img/posters/${props.poster}`} />
     <div>
-      <h3>{props.show.title}</h3>
-      <h4>({props.show.year})</h4>
-      <p>{props.show.description}</p>
-      <p>{props.foo}</p>
+      <h3>{props.title}</h3>
+      <h4>({props.year})</h4>
+      <p>{props.description}</p>
     </div>
   </div>
 );
 
-ShowCard.defaultProps = {
-  foo: 'stuff'
-};
 ShowCard.propTypes = {
-  show: shape({
-    poster: string.isRequired,
-    title: string.isRequired,
-    year: string.isRequired,
-    description: string.isRequired
-  }).isRequired,
-  foo: string.isRequired
+  poster: string.isRequired,
+  title: string.isRequired,
+  year: string.isRequired,
+  description: string.isRequired
 };
 
 export default ShowCard;
