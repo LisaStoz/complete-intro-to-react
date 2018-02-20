b5a9d4a4b19217378228e5bda948410c93312e5b
Jelizaveta Potapova <j.potapova@replyltd.co.uk>
2018-02-20 22:40:27 +0200

    filtering

--- js/Search.jsx
+++ js/Search.jsx
@@ -5,7 +5,7 @@ import preload from '../data.json';
 class Search extends Component {
 
 	state = {
-		searchTerm: 'debug statement'
+		searchTerm: ''
 	}
 	handleSearchTermChange = (event) => {
 		this.setState({searchTerm: event.target.value});
@@ -16,11 +16,14 @@ class Search extends Component {
 
 		<div className="search">
 			<header>
-				<h1>svideo</h1>
+				<h1>{this.state.searchTerm}</h1>
 				<input type="text" onChange={this.handleSearchTermChange} value={this.state.searchTerm} placeholder="search" />
 			</header>
 			<div>
-	    	{preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+	    	{preload
+					.shows
+					.filter(show => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+					.map(show => <ShowCard key={show.imdbID} {...show} />)}
 			</div>
 	  </div>
 
